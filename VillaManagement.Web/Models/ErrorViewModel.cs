80b2314622f7fb5ed14f09edb1e8ab423a2c14e9
USER <USER@DESKTOP-AIDFHSJ>
2025-06-30 15:09:54 +0600

    Add project files.

---
+++ VillaManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VillaManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
