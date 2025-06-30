3dea740b652ed849a52adbf61302c17a60014dca
arjun roy <arjunroy40202@gmail.com>
2025-07-01 00:47:36 +0600

    Design Clean architectural

---
+++ VillaManagement.Web/Controllers/VillaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaManagement.Infrastructure.Data;
+
+namespace VillaManagement.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            var villas=_db.Villas.ToList();
+            return View(villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
